831b4c3855ecec3f60e8c249e88598fccbfc8e55
icon001 <icon001@naver.com>
2018-04-10 11:08:01 +0900

    20180410 ksp 등록

---
+++ Source/Project.pas
@@ -0,0 +1,19 @@
+{ Unit created by Compile1 [TCompile] component on 2013.05.03 }
+unit Project;
+
+interface
+
+const { Do not modify these lines }
+  PROJECT_NAME: string = '';
+  EXE_FILENAME: string = 'KSP';
+  DATE_CREATED: string = '2013.05.03';
+  AUTHORS_NAME: string = '';
+  CONTACT_INFO: string = '';
+  COMPILE_DATE: string = '2018.03.13';
+  COMPILE_TIME: string = 'am 11:15:19';
+  BUILD_NUMBER: string = '789';
+  UNIQUE_IDKEY: string = ' 2018.03.13 am 11:15:19';
+
+implementation
+
+end.
